0d1f1a30318f96bf42fe463d6851345d5c721999
Juan Carlos Álvarez Viedma <juanca.alvarez.viedma@gmail.com>
2017-02-21 20:08:00 +0100

    Added functionality to control user name inputs

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname+'/public'));
+
+
+connectCounter = 0;
+
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+});
+
+io.on('connection', function(socket){
+    connectCounter++;
+    console.log('Se ha conectado un usuario('+connectCounter+').');
+    socket.broadcast.emit("new user","Se ha conectado un nuevo usuario. Usuarios totales: "+connectCounter);
+
+    socket.on("usernameInput" , function (username) {
+        socket.username= username;
+        console.log("Recibido usuario ("+socket.username+").");
+    });
+
+    socket.on("disconnect", function () {
+        connectCounter--;
+        console.log('Se ha desconectado un usuario('+connectCounter+').');
+        console.log('Se ha desconectado el usuario('+socket.username+')');
+        socket.broadcast.emit("leaving user",socket.username);
+
+    });
+
+    socket.on("mensaje", function (data) {
+        console.log("Se ha recibido el mensaje: "+data.msg+" del usuario "+data.user);
+        io.sockets.emit("newmsg", data);
+    });
+
+    socket.on("writing", function (data) {
+        io.sockets.emit("user writing", data);
+    });
+});
+
+
+http.listen(3000, function(){
+    console.log('Escuchando en puerto 3000');
+});
